441d972d96d03d7ef39e9e449b80537e74bb55c0
duanshutao <695493589@qq.com>
2017-06-30 18:06:12 +0800

    提交

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "app/model/hero";
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
+  // templateUrl: './hero-detail.component.html',
+  template: `
+<div *ngIf="hero">
+    <h2>{{hero.name}} details!</h2>
+
+</div>
+
+  `,
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero ;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
